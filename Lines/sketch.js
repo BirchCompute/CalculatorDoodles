812635ea50d61f8c2df34957c6c574d598e8f151
BirchCompute <JaronBirch@gmail.com>
2025-02-03 14:14:21 -0700

    Create sketch.js
    I threw this together real quick but I think it's done

---
+++ Lines/sketch.js
@@ -0,0 +1,30 @@
+//This program draws a bunch of lines randomly on screen
+
+
+//Makes the box bigger so you can see
+let screenScale = 5
+
+
+function setup() {
+  //TI82 screen resoltion is 96x64
+  createCanvas(96 * screenScale, 64 * screenScale);
+  
+  //Use LCD screen colors
+  background(178,194,175);
+  stroke(102,112,121)
+  
+  //Scales the line width with the screen size
+  strokeWeight(1 * screenScale);
+  
+  //My calculator ran about 8 fps with this program
+  frameRate(8);
+}
+
+
+function draw() {
+  //Repeatedly draw a line between two points placed randomly on screen
+  line( 
+    random(0,width), random(0,height), 
+    random(0,width), random(0,height) 
+  );
+}
